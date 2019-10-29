0e985875c844f63e52f620e7a222a492c624f3d1
Kim gichan <geatrigger@gmail.com>
2019-10-29 22:26:52 +0900

    Add sword asset and test sword collision

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("trigger");
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            collision.gameObject.GetComponentInParent<Animator>().SetTrigger("Damage");
+            Debug.Log("Sword trigger");
+        }
+    }
+}
