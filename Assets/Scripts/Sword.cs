0fd98662207221e48877fafd0305d67fcf5e7b49
Kim gichan <geatrigger@gmail.com>
2019-11-02 16:02:55 +0900

    Add maxRotation(commented)

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -8,29 +8,34 @@ public class Sword : MonoBehaviour
     SteamVR_Input_Sources hand;
     public SteamVR_Action_Vibration vibration;
     public GameObject rightController;
-    float maxVelocity = 1.0f;
+    float maxAngle = 10f;
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.transform.position = rightController.transform.position;
+        gameObject.transform.rotation = rightController.transform.rotation * Quaternion.Euler(90, 0, 0);
         hand = SteamVR_Input_Sources.RightHand;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //gameObject.transform.position = rightController.transform.position;
-        //gameObject.transform.rotation = rightController.transform.rotation * Quaternion.Euler(90, 0, 0);
+        gameObject.transform.position = rightController.transform.position;
+        gameObject.transform.rotation = rightController.transform.rotation * Quaternion.Euler(90, 0, 0);
         //최대속도를 유지해서 따라가는 것
-        Vector3 direction = rightController.transform.position - gameObject.transform.position;
-        if(Time.deltaTime * maxVelocity < direction.magnitude)
+        /*
+        Quaternion angle = gameObject.transform.rotation * Quaternion.Euler(-90, 0, 0) * Quaternion.Inverse(rightController.transform.rotation);
+        if(maxAngle > angle.x / Time.deltaTime && maxAngle > angle.y / Time.deltaTime && maxAngle > angle.z / Time.deltaTime)
         {
-            gameObject.transform.position = rightController.transform.position;
+            gameObject.transform.rotation = rightController.transform.rotation * Quaternion.Euler(90, 0, 0);
         }
         else
         {
-            gameObject.transform.position += direction.normalized * maxVelocity;
+            gameObject.transform.rotation = gameObject.transform.rotation *
+                Quaternion.Euler(Mathf.Max(maxAngle * Time.deltaTime, angle.x), Mathf.Max(maxAngle * Time.deltaTime, angle.y), Mathf.Max(maxAngle * Time.deltaTime, angle.z));
         }
-        gameObject.transform.rotation = rightController.transform.rotation * Quaternion.Euler(90, 0, 0);
+        gameObject.transform.position = rightController.transform.position;
+        */
     }
 
     void OnTriggerEnter(Collider collision)
