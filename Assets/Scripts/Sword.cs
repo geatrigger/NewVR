c18372a78ccc26882e828fd14705e0b9d0a6262a
Kim gichan <geatrigger@gmail.com>
2019-11-02 14:10:50 +0900

    Add vibration

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class Sword : MonoBehaviour
 {
+    public SteamVR_Input_Sources hand;
+    public SteamVR_Action_Vibration vibration;
     // Start is called before the first frame update
     void Start()
     {
+        hand = SteamVR_Input_Sources.RightHand;
     }
 
     // Update is called once per frame
@@ -21,6 +25,7 @@ public class Sword : MonoBehaviour
         if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
             collision.gameObject.GetComponentInParent<Animator>().SetTrigger("Damage");
+            vibration.Execute(0, 0.2f, 100, 1f, hand);
             Debug.Log("Sword trigger");
         }
     }
