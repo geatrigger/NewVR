731a84fdb5d8f008fa982312ebb23d950f454349
Kim gichan <geatrigger@gmail.com>
2019-11-24 15:36:40 +0900

    Fix hit effect

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using Valve.VR;
 
 public class Hand : MonoBehaviour
 {
-    public Image myImage;
 
     Animator enemyAnimator;
     SteamVR_Input_Sources hand;
@@ -51,7 +49,6 @@ public class Hand : MonoBehaviour
     void Start()
     {
 
-        //myImage.enabled = false;
         enemyAnimator = GameObject.Find("enemy").GetComponent<Animator>();
         enemyStrength = 1f; // will be changed in selection scene
         enemyGrip = 1f; // will be changed in selection scene
@@ -226,50 +223,6 @@ public class Hand : MonoBehaviour
             }
             
         }
-    }
-
-    IEnumerator fadeIn()
-    {
-        Color fadeColor = myImage.color;
-        fadeColor.a = 1.0f;
-        float time = 0f;
-        while (fadeColor.a > 0.0f)
-        {
-            time += Time.deltaTime;
-            fadeColor.a = Mathf.Lerp(1.0f, 0.0f, time*2.0f);
-            myImage.color = fadeColor;
-            yield return null;
-        }
-        fadeColor.a = 1.0f;
-        myImage.color = fadeColor;
-        myImage.enabled = false;
-    }
-    IEnumerator fadeOut()
-    {
-        Color fadeColor = myImage.color;
-        fadeColor.a = 0.0f;
-        float time = 0f;
-        while (fadeColor.a < 1.0f)
-        {
-            time += Time.deltaTime;
-            fadeColor.a = Mathf.Lerp(0.0f, 1.0f, time * 2.0f);
-            myImage.color = fadeColor;
-            yield return null;
-        }
-        fadeColor.a = 0.0f;
-        myImage.color = fadeColor;
-        //myImage.enabled = false; //페이드아웃 하고 화면 까만 상태 유지하려면 이거 키면 됨.
-        //yield return new WaitForSeconds(2.0f);
-
-        //StartCoroutine("fadeIn");
-    }
-    IEnumerator fadeInOut()
-    {
-        myImage.enabled = true;
-
-        StartCoroutine("fadeOut");
-        yield return new WaitForSeconds(0.5f);
-        StartCoroutine("fadeIn");
     }
 
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Valve.VR;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+    public Image myImage;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myImage.enabled = false;
+
     }
 
     // Update is called once per frame
@@ -25,6 +28,51 @@ public class Player : MonoBehaviour
         {
             //Debug.Log("ENEMY HIT PLAYER");
             FightingUI.addScore(false);
+            StartCoroutine("fadeInOut");
         }
     }
+    IEnumerator fadeIn()
+    {
+        Color fadeColor = myImage.color;
+        fadeColor.a = 1.0f;
+        float time = 0f;
+        while (fadeColor.a > 0.0f)
+        {
+            time += Time.deltaTime;
+            fadeColor.a = Mathf.Lerp(1.0f, 0.0f, time * 5.0f);
+            myImage.color = fadeColor;
+            yield return null;
+        }
+        fadeColor.a = 0.0f;
+        myImage.color = fadeColor;
+        myImage.enabled = false;
+    }
+    IEnumerator fadeOut()
+    {
+        Color fadeColor = myImage.color;
+        fadeColor.a = 0.0f;
+        float time = 0f;
+        while (fadeColor.a < 1.0f)
+        {
+            time += Time.deltaTime;
+            fadeColor.a = Mathf.Lerp(0.0f, 1.0f, time * 5.0f);
+            myImage.color = fadeColor;
+            yield return null;
+        }
+        fadeColor.a = 1.0f;
+        myImage.color = fadeColor;
+        //myImage.enabled = false; //페이드아웃 하고 화면 까만 상태 유지하려면 이거 키면 됨.
+        //yield return new WaitForSeconds(2.0f);
+
+        //StartCoroutine("fadeIn");
+    }
+    IEnumerator fadeInOut()
+    {
+        myImage.enabled = true;
+
+        StartCoroutine("fadeOut");
+        yield return new WaitForSeconds(0.3f);
+        StartCoroutine("fadeIn");
+    }
+
 }
